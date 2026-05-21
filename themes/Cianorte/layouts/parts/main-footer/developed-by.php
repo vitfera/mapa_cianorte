686c2fd2ffa5afd0e498043f91a27752f8035c2e
Victor Ferreira <vitfera@hotmail.com>
2026-05-21 18:35:10 -0300

    adiciona tema cianorte

---
+++ themes/Cianorte/layouts/parts/main-footer/developed-by.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+use MapasCulturais\i;
+
+$this->import('mc-icon');
+?>
+<p>
+    <?php i::_e("plataforma criada pela comunidade") ?> 
+    <span class="mapas"> <mc-icon name="map"></mc-icon><?php i::_e("mapas culturais"); ?> </span>
+</p>
