d94cc3b8fe5b62f3f9e6867655a0754520f32f99
Victor Ferreira <vitfera@hotmail.com>
2026-06-04 02:11:32 -0300

    atualizando siteName

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -1,8 +1,8 @@
 <?php 
 
 return [
-    'app.siteName' => 'Mapas Culturais Base Project',
-    'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
+    'app.siteName' => 'Mapa Cultural Cianorte',
+    'app.siteDescription' => 'O Mapa Cultural Cianorte é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
     'themes.active' => 'Cianorte',
